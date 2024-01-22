f81dae6af20a7f52a85927e5f52941cc3067b629
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-22 16:36:44 +0000

    fix: error handling

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    console.log(data);
+  if (err) {
+    console.error('Error reading the file:', err);
+  } else {
+    console.log('File content:\n', data);
+  }
 });

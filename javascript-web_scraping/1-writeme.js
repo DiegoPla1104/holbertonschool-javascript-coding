ede77f9de0f51114cf7d3b1262f33757768043a1
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-22 16:50:24 +0000

    fix?

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,14 +4,9 @@ const fs = require('fs');
 const filename = process.argv[2];
 const text = process.argv[3];
 
-fs.readFile(filename, 'utf-8', (err, data) => {
-  if (err) {
-    console.error('Error reading the file:', err);
-  } else {
-    fs.writeFile(filename, text, 'utf-8', (writeErr) => {
-      if (writeErr) {
-        console.error('Error writing to the file:', writeErr);
-      }
-      });
-  }
+
+fs.writeFile(filename, text, 'utf-8', (writeErr) => {
+  if (writeErr) {
+    console.error('Error writing to the file:', writeErr);
+    }
 });

6004e907548a5643cfee9f472375b0b8ba273228
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-22 16:45:20 +0000

    feat: task 1 and fix: some minor tweaks

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,11 +2,16 @@
 const fs = require('fs');
 
 const filename = process.argv[2];
+const text = process.argv[3];
 
 fs.readFile(filename, 'utf-8', (err, data) => {
   if (err) {
     console.error('Error reading the file:', err);
   } else {
-    console.log(data);
+    fs.writeFile(filename, text, 'utf-8', (writeErr) => {
+      if (writeErr) {
+        console.error('Error writing to the file:', writeErr);
+      }
+      });
   }
 });

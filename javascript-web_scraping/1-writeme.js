922d9428dc7ea1d353f5da6f10eb6d678ce22235
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-22 16:52:08 +0000

    fix: semistandard validation

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const filename = process.argv[2];
 const text = process.argv[3];
 
-
 fs.writeFile(filename, text, 'utf-8', (writeErr) => {
   if (writeErr) {
     console.error('Error writing to the file:', writeErr);

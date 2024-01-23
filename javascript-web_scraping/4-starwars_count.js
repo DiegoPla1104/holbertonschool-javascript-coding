865827fce35afaa5a813a684dc2a30605b274400
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-23 16:56:50 +0000

    feat: task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  const jsonResponse = JSON.parse(body);
+  let movies = 0;
+
+  for (let i = 0; i < jsonResponse.results.length; i++) {
+    const characters = jsonResponse.results[i].characters;
+
+    for (let j = 0; j < characters.length; j++) {
+      if (characters[j].includes('18')) {
+        movies += 1;
+        break; // No need to continue checking if found
+      }
+    }
+  }
+  console.log(movies);
+});

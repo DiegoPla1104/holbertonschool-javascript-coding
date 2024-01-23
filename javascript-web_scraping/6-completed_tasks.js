01f49cf8d11facb62e0ebdf4b6c4016de78bb31e
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-23 17:37:23 +0000

    fix: returned to request module

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
-const axios = require('axios');
+const request = require('request');
 const url = process.argv[2];
 
-axios.get(url)
-  .then(response => {
-    const jsonResponse = response.data;
-    const tasksdict = {};
-
-    for (let i = 0; i < jsonResponse.length; i++) {
-      const task = jsonResponse[i].completed;
-      const userid = jsonResponse[i].userId;
-      if (task === true) {
-        tasksdict[userid] = (tasksdict[userid] || 0) + 1;
-      }
-    }
-    console.log(tasksdict);
-  })
-  .catch(error => {
+request.get(url, (error, response, body) => {
+  if (error) {
     console.error(error);
-  });
+  }
+  const jsonResponse = JSON.parse(body);
+  const tasksdict = {};
+
+  for (let i = 0; i < jsonResponse.length; i++) {
+    const task = jsonResponse[i].completed;
+    const userid = jsonResponse[i].userId;
+    if (task === true) {
+      tasksdict[userid] = (tasksdict[userid] || 0) + 1;
+    }
+  }
+  console.log(tasksdict);
+});
